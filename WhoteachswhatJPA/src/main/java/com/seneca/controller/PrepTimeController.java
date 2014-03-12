f4028bb91335e3482fd8b05c37b98af84286fdb3
Zakeria Hassan <zak.hassan1010@gmail.com>
2014-03-12 14:40:41 -0400

    added insert script for mysql

--- WhoteachswhatJPA/src/main/java/com/seneca/controller/PrepTimeController.java
+++ WhoteachswhatJPA/src/main/java/com/seneca/controller/PrepTimeController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,7 +31,9 @@ public class PrepTimeController {
 		private PrepTimeService prepTimeService;
 		
 		@RequestMapping(value = "/viewPrepTime", method = RequestMethod.GET)
-		public String view() {
+		public String view(ModelMap model) {
+		//	model.addAttribute("", attributeValue);
+			
 			return "PrepTime/viewPrepTime";
 		}
 
