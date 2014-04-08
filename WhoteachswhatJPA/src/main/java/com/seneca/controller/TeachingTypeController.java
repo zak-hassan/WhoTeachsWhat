6d5af082b3a17fee44b530244b20e1dfd93be206
Anil Santokhi <anil.d.santokhi@gmail.com>
2014-04-08 02:19:47 -0400

    Javadocs for TeachingTypeController

--- WhoteachswhatJPA/src/main/java/com/seneca/controller/TeachingTypeController.java
+++ WhoteachswhatJPA/src/main/java/com/seneca/controller/TeachingTypeController.java
@@ -16,20 +16,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.seneca.model.TeachingType;
 import com.seneca.service.TeachingTypeService;
 
+/**
+ * This class is the controller which regulates all evalfactor operations.
+ * 
+ * @author Anil Santokhi <anil.d.santokhI@gmail.com>, Zak Hassan <zak.hassan1010@gmail.com>
+ * @lastmodified April 7, 2014
+ * @version 1.0
+ */
+
 @Controller
 public class TeachingTypeController {
-
-	/**
-	 * This class is the controller which regulates all evalfactor operations.
-	 * 
-	 * @author Anil Santokhi <anil.d.santokhI@gmail.com>
-	 * @lastmodified March 24, 2014
-	 * @version 0.0.1
-	 */
 
 	@Autowired
 	private TeachingTypeService teachingTypeService;
 
+	/**
+	 * This method accepts no parameters and maps the URL '/manageTeachingType' to a view page
+	 * 
+	 * @see
+	 * 		com.seneca.model.TeachingType
+	 * @return
+	 * 		The view page to be rendered, along with a List of all TeachingTypes through the ModelMap
+	 */
 	@RequestMapping(value = "/manageTeachingType", method = RequestMethod.GET)
 	public String view(ModelMap model) {
 		model.addAttribute("allTeachingTypes", teachingTypeService.getAll());
@@ -40,23 +48,15 @@ public class TeachingTypeController {
 	// REST API ENDPOINTS:
 
 	/**
-	 * This method accepts data posted from the UpdateCourseForm and updates a
-	 * course using the appropriate service method
+	 * This method is accessed through a POST request and allows the creation of a TeachingType
 	 * 
-	 * @see com.seneca.service.CourseService
-	 * 
-	 * @param courseCode
-	 *            The six to eight digit course code
-	 * @param courseName
-	 *            The name of the course
-	 * @param crossoverCourse
-	 *            The course code that is syncronomous with this course code
-	 * @param oldCourse
-	 *            The previous course code of the course, if any
-	 * 
-	 * @return A String containing the name of the view to render
+	 * @see 
+	 * 		com.seneca.service.TeachingTypeService
+	 * @param teachingType_name
+	 * 		The name of the teaching type
+	 * @return 
+	 * 		A HashMap containing the TeachingType id and a message containing the success of the operation in JSON
 	 */
-
 	@RequestMapping(value = "/api/teachingType", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody
 	Map<String, String> listAddJSON(
@@ -71,37 +71,18 @@ public class TeachingTypeController {
 		return list;
 	}
 
-	@RequestMapping(value = "/api/teachingType/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody
-	Map<String, String> listGetOneJSON(@PathVariable("id") Integer id) {
-		
-		Map<String, String> list = new HashMap<String, String>();
-		TeachingType t = teachingTypeService.getOne(id); // Seems to be null
-		if (t != null) {
-			list.put("teachingType_name", t.getTeachingType_name());
-			list.put("id", t.getTeachingType_id() + "");
-		} 
-		return list;
-	}
-	
 	/**
-	 * This method accepts data posted from the UpdateCourseForm and updates a
-	 * course using the appropriate service method
+	 * This method is accessed through a POST request and allows the updating of a TeachingType
 	 * 
-	 * @see com.seneca.service.CourseService
-	 * 
-	 * @param courseCode
-	 *            The six to eight digit course code
-	 * @param courseName
-	 *            The name of the course
-	 * @param crossoverCourse
-	 *            The course code that is synonomous with this course code
-	 * @param oldCourse
-	 *            The previous course code of the course, if any
-	 * 
-	 * @return A String containing the name of the view to render
+	 * @see 
+	 * 		com.seneca.service.TeachingTypeService
+	 * @param id
+	 * 		Uniquely identifies the object
+	 * @param teachingType_name
+	 * 		The name of the teaching type
+	 * @return 
+	 * 		A HashMap containing the success of the operation in JSON
 	 */
-
 	@RequestMapping(value = "/api/teachingType/{id}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody
 	Map<String, String> listUpdateJSON(
@@ -115,23 +96,14 @@ public class TeachingTypeController {
 	}
 
 	/**
-	 * This method accepts data posted from the UpdateCourseForm and updates a
-	 * course using the appropriate service method
+	 * This method is accessed through a DELETE request and allows the deleting of a TeachingType
 	 * 
-	 * @see com.seneca.ser ourseService
-	 * 
-	 * @param courseCode
-	 *            The six to eight digit course code
-	 * @param courseName
-	 *            The name of the course
-	 * @param crossoverCourse
-	 *            The course code that is synonomous with this course code
-	 * @param oldCourse
-	 *            The previous course code of the course, if any
-	 * 
-	 * @return A String containing the name of the view to render
+	 * @see 
+	 * 		com.seneca.service.TeachingTypeService
+	 * @param id
+	 * 		 Uniquely identifies the object
+	 * @return A HashMap containing the success of the operation in JSON
 	 */
-
 	@RequestMapping(value = "/api/teachingType/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody
 	Map<String, String> listDeleteJSON(@PathVariable("id") Integer id) {
